b15ae96990ff847a21b32f1b36edec09277ae15f
Koki Takahashi <hakatasiloving@gmail.com>
2025-02-14 16:06:13 +0900

    Implement clause processing and show by time

--- src/lib/QuizStatement.tsx
+++ src/lib/QuizStatement.tsx
@@ -1,5 +1,7 @@
+import {createMemo, Show} from 'solid-js';
 import styles from './QuizStatement.module.css';
 import type {Timepoint} from './schema.ts';
+import {clamp} from 'lodash-es';
 
 interface QuizStatementProps {
 	clauses: string[];
@@ -7,11 +9,54 @@ interface QuizStatementProps {
 	timepoints: Timepoint[];
 }
 
+const extractMarkIndex = (markName: string | null | undefined) =>
+	markName === null || markName === undefined
+		? Number.NaN
+		: Number.parseInt(markName.match(/c(\d+)/)?.[1] ?? '');
+
+interface ClauseInformation {
+	text: string;
+	duration: number;
+	ellapsedTime: number;
+}
+
 const QuizStatement = (props: QuizStatementProps) => {
+	const sortedTimepoints = createMemo(() =>
+		props.timepoints.sort(
+			(a, b) => extractMarkIndex(a.markName) - extractMarkIndex(b.markName),
+		),
+	);
+
+	const clauseInformation = createMemo(() => {
+		const outputs: ClauseInformation[] = [];
+		let offset = 0;
+
+		for (const timepoint of sortedTimepoints()) {
+			const markIndex = extractMarkIndex(timepoint.markName);
+			const clause = props.clauses[markIndex];
+			const duration = (timepoint.timeSeconds ?? 0) - offset;
+
+			outputs.push({
+				text: clause,
+				duration,
+				ellapsedTime: clamp(props.ellapsedTime - offset, 0, duration),
+			});
+
+			offset = timepoint.timeSeconds ?? 0;
+		}
+
+		return outputs;
+	});
+
 	return (
 		<div class={styles.quiz}>
-			{props.clauses.map((token) => (
-				<span class={styles.quiz_token}>{token}</span>
+			{clauseInformation().map((clauseInfo, clauseIndex) => (
+				<Show when={clauseInfo.ellapsedTime > 0}>
+					<Show when={clauseIndex !== 0}>
+						<wbr />
+					</Show>
+					<span class={styles.quiz_token}>{clauseInfo.text}</span>
+				</Show>
 			))}
 		</div>
 	);
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,43 @@
-import type {Component} from 'solid-js';
-import {Quizzes} from '~/lib/firebase';
-import {useFirestore} from 'solid-firebase';
+import {createSignal, onMount, type Component} from 'solid-js';
+import {Quizzes, storage} from '~/lib/firebase';
+import {useDownloadURL, useFirestore} from 'solid-firebase';
 import {doc} from 'firebase/firestore';
+import {ref} from 'firebase/storage';
 import QuizStatement from '~/lib/QuizStatement';
 import Doc from '~/lib/Doc';
 
 const Index: Component = () => {
 	const quiz = useFirestore(doc(Quizzes, 'it-000000'));
+	const quizQuestionDataUrl = useDownloadURL(
+		ref(storage, 'quiz/it-000000/question.mp3'),
+	);
+
+	const [ellapsedTime, setEllapsedTime] = createSignal(0);
+
+	let audioElement!: HTMLAudioElement;
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			setEllapsedTime((prev) => prev + 0.1);
+		}, 100);
+
+		audioElement.play();
+
+		return () => clearInterval(interval);
+	});
 
 	return (
 		<div>
+			<audio controls={true} ref={audioElement}>
+				<source src={quizQuestionDataUrl()} type="audio/mpeg" />
+				<track kind="captions" />
+			</audio>
 			<Doc data={quiz}>
 				{(data) => (
 					<div>
 						<QuizStatement
 							clauses={data.clauses}
-							ellapsedTime={3.5}
+							ellapsedTime={ellapsedTime()}
 							timepoints={data.timepoints}
 						/>
 					</div>
